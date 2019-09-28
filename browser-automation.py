ea6b9c86400cbed2abab4060c8420a94c23115ed
H4mid2019 <47919732+H4mid2019@users.noreply.github.com>
2019-09-29 01:29:16 +0300

    Add files via upload

---
+++ browser-automation.py
@@ -0,0 +1,29 @@
+import config
+from selenium import webdriver
+import time
+import pyotp
+
+
+totp = pyotp.TOTP(config.otpkey)
+browser = webdriver.Chrome()
+browser.get(config.site)
+
+
+def login_sele():
+    username_box = browser.find_element_by_id("login_field")
+    username_box.send_keys(config.user)
+    password_box = browser.find_element_by_id("password")
+    password_box.send_keys(config.password)
+    password_box.submit()
+    time.sleep(1)
+    twofactor = browser.find_element_by_name("otp")
+    twofactor.send_keys(totp.now())
+    twofactor.submit()
+
+
+if __name__ == "__main__":
+    try:
+        login_sele()
+    except:
+        print("An error occurred")
+        webdriver.Chrome.quit()
---
+++ config.py
@@ -0,0 +1,4 @@
+site = "https://github.com/login"
+user = "username"
+password = "your_password"
+otpkey = "one_time_secret"
